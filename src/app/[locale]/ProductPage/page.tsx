046ba79078441a07cdc83b6ee91521d1f03b3e86
Juan Carlos <jcarlos61200@gmail.com>
2024-10-21 20:25:34 -0600

    final v.2

--- src/app/[locale]/ProductPage/page.tsx
+++ src/app/[locale]/ProductPage/page.tsx
@@ -1,16 +1,19 @@
 'use client'; // Esto asegura que el componente se ejecute en el cliente
 
 import React, { useState } from 'react';
-import { FaShareAlt, FaStar, FaTruck } from 'react-icons/fa';
+import { FaSave, FaShareAlt, FaStar, FaTruck } from 'react-icons/fa';
+import { FaBookmark } from 'react-icons/fa6';
+
 
 // Carrusel de "More Products"
-const moreProductsImages = [
+// Carrusel personalizado (CustomCarousel)
+const images = [
   'https://symposium.pacificcollege.edu/wp-content/uploads/2021/02/Kiiko-02-1.jpg',
   'https://www.qiology.com.au/wp-content/uploads/2019/05/Kiiko-866x1024.jpg',
   'https://altaivida.net/wp-content/uploads/2017/01/kiiko-matsumoto-altaivida-e1483914916971-300x273.jpeg',
   'https://m.media-amazon.com/images/I/A1aTrRY3W7L._AC_UF894,1000_QL80_.jpg',
   'https://m.media-amazon.com/images/I/81dB8Q2iqqL._AC_CR0%2C0%2C0%2C0_SY315_.jpg',
-  'https://www.easterncurrents.ca/wp-content/uploads/2020/01/B5760.jpg'
+  'https://www.easterncurrents.ca/wp-content/uploads/2020/01/B5760.jpg',
 ];
 
 const CustomCarousel: React.FC = () => {
@@ -18,48 +21,53 @@ const CustomCarousel: React.FC = () => {
 
   const prevSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? moreProductsImages.length - 3 : prevIndex - 1
+      prevIndex === 0 ? images.length - 3 : prevIndex - 1
     );
   };
 
   const nextSlide = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex >= moreProductsImages.length - 3 ? 0 : prevIndex + 1
+      prevIndex >= images.length - 3 ? 0 : prevIndex + 1
     );
   };
 
   return (
-    <div className="relative w-full max-w-5xl mx-auto mt-0 mb-40 px-4">
-      <h1 className="text-[#5b737b] text-2xl md:text-3xl font-bold mb-10 text-center">
-        MORE PRODUCTS
-      </h1>
-      <div className="flex overflow-hidden justify-center">
-        {moreProductsImages
-          .slice(currentIndex, currentIndex + 3)
-          .map((image, index) => (
-            <img
-              key={index}
-              src={image}
-              alt={`Imagen ${currentIndex + index + 1}`}
-              className="w-[250px] h-[250px] md:w-[400px] md:h-[400px] object-cover rounded-lg mx-2"
-            />
-          ))}
-      </div>
+    <div className="relative w-full mx-1 mt-8 mb-40 p-6 md:p-32">
+  <h1 className="text-[#004F44] font-montserrat font-normal text-[40px]  leading-[48.76px] tracking-[0.5px]">
+      MORE PRODUCTS
+    </h1>
+  <div className="flex items-center gap-4 mt-10 "> {/* Alineación de las imágenes y el texto "more-->" */}
+    <div className="flex overflow-hidden gap-4"> {/* Añadimos un gap entre imágenes */}
+      {images.slice(currentIndex, currentIndex + 3).map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Imagen ${currentIndex + index + 1}`}
+          className="w-[550px] h-[466.78px] object-cover opacity-100"
+          style={{
+            top: '2018.11px',
+            left: '170.4px',
+            opacity: '1',
+          }}
+        />
+      ))}
+    </div>
 
-      {/* Botones de navegación */}
+    {/* Texto para cambiar imágenes centrado verticalmente con las imágenes */}
+    <div className="flex items-center justify-center">
       <button
-        onClick={prevSlide}
-        className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-full"
+        onClick={nextSlide} // Función que avanza al siguiente conjunto de imágenes
+        className="text-[#5b737b] font-bold cursor-pointer"
       >
-        &#8249;
-      </button>
-      <button
-        onClick={nextSlide}
-        className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-black text-white px-2 py-1 rounded-full"
-      >
-        &#8250;
+        more➔
       </button>
     </div>
+  </div>
+</div>
+
+  
+  
+  
   );
 };
 
@@ -117,119 +125,176 @@ const ProductPage: React.FC = () => {
           </div>
         </div>
 
-        {/* Columna derecha - Información del producto */}
-        <div className="flex flex-col items-center md:items-start text-center md:text-left">
-          {/* Nombre del producto y descripción corta */}
-          <div className="flex justify-between items-center mb-4 w-full max-w-lg">
-            <div>
-              <h1 className="text-3xl md:text-4xl font-bold text-[#5b737b]">
-                Nombre del Producto
-              </h1>
-              <p className="text-sm text-gray-500">Product Description</p>
-            </div>
-            <FaShareAlt className="text-[#5b737b] cursor-pointer" size={24} />
-          </div>
 
-          {/* Precio y calificación */}
-          <div className="flex justify-between items-center mb-4 w-full max-w-lg">
-            <div>
-              <p className="text-sm text-gray-500 line-through">$59.99</p>
-              <p className="text-2xl font-semibold text-gray-600">$49.99</p>
-            </div>
-            <div className="flex items-center">
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-yellow-500" />
-              <FaStar className="text-gray-300" />
-              <span className="ml-2 text-gray-600">4.8 (50 Reviews)</span>
-            </div>
-          </div>
 
-          {/* Choose a color */}
-          <div className="mb-4 w-full max-w-lg">
-            <h2 className="text-xl font-semibold mb-2">Choose a color:</h2>
-            <div className="flex justify-center md:justify-start space-x-4">
-              {['Red', 'Blue', 'Green', 'Yellow'].map((color) => (
-                <div
-                  key={color}
-                  className={`w-10 h-10 rounded-full cursor-pointer border-2 ${
-                    selectedColor === color
-                      ? 'border-[#004f44]'
-                      : 'border-gray-300'
-                  }`}
-                  style={{ backgroundColor: color.toLowerCase() }}
-                  onClick={() => handleColorChange(color)}
-                />
-              ))}
-            </div>
-          </div>
 
-          {/* Choose a size con radio buttons */}
-          <div className="mb-4 w-full max-w-lg">
-            <h2 className="text-xl font-semibold mb-2">Choose a size:</h2>
-            <div className="flex justify-center md:justify-start space-x-4">
-              {['Small', 'Medium', 'Large', 'Extra Large'].map((size) => (
-                <label
-                  key={size}
-                  className="inline-flex items-center space-x-2"
-                >
-                  <input
-                    type="radio"
-                    name="size"
-                    value={size}
-                    checked={selectedSize === size}
-                    onChange={() => handleSizeChange(size)}
-                    className="form-radio text-[#004f44]"
-                  />
-                  <span>{size}</span>
-                </label>
-              ))}
-            </div>
-          </div>
-
-          {/* Choose quantity */}
-          <div className="mb-4 w-full max-w-lg flex justify-center md:justify-start items-center space-x-4">
-            <div className="flex items-center space-x-2">
-              <button
-                onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
-                className="px-2 py-1 bg-gray-200 rounded-md"
-              >
-                -
-              </button>
-              <input
-                type="number"
-                value={quantity}
-                min="1"
-                onChange={handleQuantityChange}
-                className="w-12 text-center border border-gray-300 rounded-md"
-              />
-              <button
-                onClick={() => setQuantity(quantity + 1)}
-                className="px-2 py-1 bg-gray-200 rounded-md"
-              >
-                +
-              </button>
-            </div>
-            <button className="bg-[#004f44] text-white px-6 py-2 rounded-lg font-bold hover:bg-[#003833]">
-              Add to Cart
-            </button>
-          </div>
-
-          {/* Free delivery y link de código postal */}
-          <div className="p-8 bg-white border rounded-lg mb-4 w-full max-w-lg">
-            <div className="flex items-center space-x-2">
-              <FaTruck className="text-[#004f44]" size={24} />
-              <span className="text-sm font-bold text-gray-600">
-                Free Delivery
-              </span>
-            </div>
-            <p className="text-sm text-[#004f44] underline cursor-pointer mt-2">
-              Enter your postal code for delivery availability
-            </p>
-          </div>
-        </div>
+  {/* Columna derecha - Información del producto */}
+<div className="flex flex-col items-center md:items-start text-center md:text-left flex-grow">
+  
+  {/* Nombre del producto y descripción corta */}
+  <div className="flex justify-between items-center mb-4 w-full max-w-lg border-b border-[#E4E4E4] pb-4">
+    <div>
+      <h1 className="text-3xl md:text-4xl font-bold text-[#000000]">Product</h1>
+      <p className="text-sm text-gray-500">Product Description</p>
+    </div>
+    {/* Iconos de guardado y compartir */}
+    <div className="flex space-x-2 justify-end">
+      <div 
+        className="bg-[#EDF0F8] p-2 rounded-xl inline-flex items-center justify-center"
+        style={{ width: '40px', height: '40px' }} 
+      >
+        <FaBookmark className="text-[#5b737b] cursor-pointer" size={24} /> 
       </div>
+      <div 
+        className="bg-[#EDF0F8] p-2 rounded-xl inline-flex items-center justify-center"
+        style={{ width: '40px', height: '40px' }} 
+      >
+        <FaShareAlt className="text-[#5b737b] cursor-pointer" size={24} />
+      </div>
+    </div>
+  </div>
+
+  {/* Precio y calificación */}
+  <div className="flex justify-between items-center mb-4 w-full max-w-lg border-b border-[#E4E4E4] pb-4">
+    <div>
+    <p className="text-2xl font-semibold text-gray-600">$49.99</p>
+      <p className="text-sm text-gray-500 line-through">$59.99</p>
+      
+    </div>
+    <div className="flex items-center space-x-2">
+  {/* Óvalo para calificación */}
+  <div className="flex items-end bg-[#FBF3EA] p-1 rounded-3xl" style={{ width: '60px' }}> {/* Ancho fijo */}
+    <span className="text-[#e0c8a2]" style={{ fontSize: '18px' }}>☆</span>
+    <span className="ml-1 text-gray-600 text-sm">4.8</span>
+  </div>
+
+  {/* Óvalo para reseñas */}
+  <div className="flex items-center bg-[#EDF0F8] p-1.5 rounded-full" style={{ width: '130px' }}> {/* Ancho fijo */}
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-4 w-4 text-[#5b737b]"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={1.5}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M8 10h.01M12 10h.01M16 10h.01M21 16V8a2 2 0 00-2-2H5a2 2 0 00-2 2v8a2 2 0 002 2h14l4 4z"
+      />
+    </svg>
+    <span className="ml-1 text-gray-600 text-sm">(50 Reviews)</span>
+  </div>
+</div>
+</div>
+
+  {/* Choose a color */}
+  <div className="mb-4 w-full max-w-lg border-b border-[#E4E4E4] pb-4">
+    <h2 className="text-xl font-semibold mb-2">Choose a color:</h2>
+    <div className="flex justify-center md:justify-start space-x-4">
+      {['Red', 'Blue', 'Green', 'Yellow'].map((color) => (
+        <div
+          key={color}
+          className={`w-10 h-10 rounded-full cursor-pointer border-2 ${selectedColor === color ? 'border-[#004f44]' : 'border-gray-300'}`}
+          style={{ backgroundColor: color.toLowerCase() }}
+          onClick={() => handleColorChange(color)}
+        />
+      ))}
+    </div>
+  </div>
+
+  {/* Choose a size con radio buttons  Choose quantity */}
+  <div className="mb-4 w-full max-w-lg border-b border-[#E4E4E4] pb-4">
+  <h2 className="text-xl font-semibold mb-2">Choose a size:</h2>
+  <div className="flex justify-center md:justify-start space-x-2"> {/* Espacio reducido entre óvalos */}
+    {['Small', 'Medium', 'Large', 'Extra Large', 'XXL'].map((size) => (
+      <div
+        key={size}
+        className="bg-[#EDF0F8] rounded-full px-3 py-0.5" // Aún más delgado
+      >
+        <label className="inline-flex items-center space-x-1"> {/* Espacio reducido */}
+          <input
+            type="radio"
+            name="size"
+            value={size}
+            checked={selectedSize === size}
+            onChange={() => handleSizeChange(size)}
+            className="form-radio text-[#004f44]"
+          />
+          <span className="text-xs">{size}</span> {/* Tamaño de texto más pequeño */}
+        </label>
+      </div>
+    ))}
+  </div>
+</div>
+
+
+
+
+{/* Choose quantity */}
+
+
+
+
+{/* Choose quantity with oval background only for the counter */}
+<div className="mb-4 w-full max-w-lg flex justify-center md:justify-start items-center space-x-4">
+  <div className="bg-[#F3F3F3] rounded-full p-3 inline-flex items-center space-x-2">
+    <button
+      onClick={() => setQuantity(quantity > 1 ? quantity - 1 : 1)}
+      className="px-2 py-1 bg-gray-200 rounded-md"
+    >
+      -
+    </button>
+    <input
+      type="number"
+      value={quantity}
+      min="1"
+      onChange={handleQuantityChange}
+      className="w-12 text-center border border-gray-300 rounded-md"
+    />
+    <button
+      onClick={() => setQuantity(quantity + 1)}
+      className="px-2 py-1 bg-gray-200 rounded-md"
+    >
+      +
+    </button>
+  </div>
+
+
+
+
+
+
+
+    
+    <button
+  className="bg-[#5B747B] text-white font-bold hover:bg-[#003833] rounded-3xl"
+  style={{ width: '250px', height: '59px' }} // Ajuste del tamaño
+>
+  Add to Cart
+</button>
+  </div>
+
+  {/* Free delivery y link de código postal */} 
+  <div className="mt-auto p-8 bg-white border rounded-lg mb-4 w-full max-w-lg">
+    <div className="flex items-center space-x-2">
+      <FaTruck className="text-[#004f44]" size={24} />
+      <span className="text-sm font-bold text-gray-600">
+        Free Delivery
+      </span>
+    </div>
+    <p className="text-sm text-[#004f44] underline cursor-pointer mt-2">
+      Enter your postal code for delivery availability
+    </p>
+  </div>
+</div>
+</div>
+
+
+
+
+      
 
       {/* Sección de pestañas debajo de la información del producto */}
       <div className="col-span-2 mt-10 justify-center flex flex-col items-center md:items-center">
--- src/app/[locale]/listProducts/page.tsx
+++ src/app/[locale]/listProducts/page.tsx
@@ -13,33 +13,73 @@ const productImages = [
 const ListProductsPage: React.FC = () => {
   return (
     <>
-      <div className="w-full bg-white p-32">
-        <div className="flex justify-between items-center">
-          <h1 className="text-[#5b737b] text-3xl font-bold">ALL PRODUCTS</h1>
-          <Link href="/ProductPage" className="text-[#5b737b] underline">View All</Link>
+       <div className="w-full bg-white p-6 md:p-32">
+  <div className="flex justify-between items-center">
+  <h1 
+  className="text-[#004F44]" 
+  style={{ 
+    fontFamily: 'Montserrat', 
+    fontSize: '40px', 
+    fontWeight: 400, 
+    lineHeight: '48.76px', 
+    letterSpacing: '0.5px', 
+    textAlign: 'left' 
+  }}
+>
+  ALL PRODUCTS
+</h1>
+
+    <Link href="/listProducts" className="text-[#5b737b] underline">
+      View All
+    </Link>
+  </div>
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mt-4">
+    {Array.from({ length: 64 }, (_, index) => (
+      <Link
+        key={index}
+        href={`/productPage?id=${index + 1}`}
+        className="text-center bg-white p-4 rounded-lg"
+      >
+        {/* Imagen con tamaño especificado y sin borde redondeado */}
+        <img
+          src={`https://m.media-amazon.com/images/I/81dB8Q2iqqL._AC_CR0%2C0%2C0%2C0_SY315_.jpg`}
+          alt={`Producto ${index + 1}`}
+          className="w-[355px] h-[360px] object-cover cursor-pointer"
+        />
+
+        {/* Nombre del producto y precio alineados */}
+        <div className="flex justify-between items-center mt-4">
+          <p className="text-[#004F44] text-left" style={{ width: '355px' }}> {/* Alineado al borde de la imagen */}
+            Producto {index + 1}
+          </p>
+          <p className="text-[#5b737b] font-bold text-right">
+            ${(29.99 + index * 10).toFixed(1)}
+          </p>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mt-4">
-          {Array.from({ length: 64 }, (_, index) => (
-            <div key={index} className="text-center bg-white p-4 rounded-lg shadow-md">
-              <Link href={`/productPage?id=${index + 1}`}>
-                <img
-                  src={productImages[index % productImages.length]}
-                  alt={`Producto ${index + 1}`}
-                  className="w-full h-48 object-cover rounded cursor-pointer"
-                />
-              </Link>
-              <p className="text-[#5b737b] mt-4">Producto {index + 1} - ${29.99 + index * 5}</p>
-              <label className="text-[#5b737b] mt-2" htmlFor={`color${index + 1}`}>Variante:</label>
-              <select id={`color${index + 1}`} className="mt-1 mb-2">
-                <option value="color1">Color 1</option>
-                <option value="color2">Color 2</option>
-                <option value="color3">Color 3</option>
-              </select>
-              <button className="bg-[#004f44] text-white px-4 py-2 mt-2 rounded">Buy</button>
+
+        {/* Variante y círculos de colores alineados */}
+        <div className="flex justify-between items-center mt-2">
+          <div className="text-left" style={{ width: '355px' }}> {/* Alineado al borde de la imagen */}
+            <label className="text-[#9EA9A7]" htmlFor={`color${index + 1}`}>
+              Variante:
+            </label>
+            <div className="flex space-x-2 mt-1">
+              <div className="w-4 h-4 bg-red-500 rounded-full border"></div>
+              <div className="w-4 h-4 bg-blue-500 rounded-full border"></div>
+              <div className="w-4 h-4 bg-green-500 rounded-full border"></div>
+              <div className="w-4 h-4 bg-yellow-500 rounded-full border"></div>
             </div>
-          ))}
+          </div>
+
+          {/* Botón Buy alineado al borde derecho de la imagen */}
+          <button className="bg-[#004f44] text-white px-4 py-2 text-right" style={{ marginLeft: 'auto' }}>
+            Buy
+          </button>
         </div>
-      </div>
+      </Link>
+    ))}
+  </div>
+</div>
     </>
   );
 };
