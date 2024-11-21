bd3d2a47ab5feb7231b677f85ece0cf2a4e3a503
landrescreative <landres.creative@gmail.com>
2024-11-21 02:52:29 -0600

    test

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,7 +7,6 @@ import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 import Preloader from '@/components/Preloader';
 import StickyBanner from '@/components/StickyBanner';
-import MyComponent from '@/components/MyComponent';
 
 export const metadata: Metadata = {
   title: 'Design Medicine - Seminar',
@@ -25,7 +24,15 @@ interface LocaleLayoutProps {
 export default function LocaleLayout({ children, params }: LocaleLayoutProps) {
   return (
     <html lang={params.locale}>
-      <MyComponent />
+      <head>
+        <script
+          nitro-exclude
+          type="text/javascript"
+          id="sa-dynamic-optimization"
+          data-uuid="30c99e62-9faf-445c-aa56-91e145fb696e"
+          src="data:text/javascript;base64,dmFyIHNjcmlwdCA9IGRvY3VtZW50LmNyZWF0ZUVsZW1lbnQoInNjcmlwdCIpO3NjcmlwdC5zZXRBdHRyaWJ1dGUoIm5vd3Byb2NrZXQiLCAiIik7c2NyaXB0LnNldEF0dHJpYnV0ZSgibml0cm8tZXhjbHVkZSIsICIiKTtzY3JpcHQuc3JjID0gImh0dHBzOi8vZGFzaGJvYXJkLmxpbmtncmFwaC5jb20vc2NyaXB0cy9keW5hbWljX29wdGltaXphdGlvbi5qcyI7c2NyaXB0LmRhdGFzZXQudXVpZCA9ICIzMGM5OWU2Mi05ZmFmLTQ0NWMtYWE1Ni05MWUxNDVmYjY5NmUiO3NjcmlwdC5pZCA9ICJzYS1keW5hbWljLW9wdGltaXphdGlvbi1sb2FkZXIiO2RvY3VtZW50LmhlYWQuYXBwZW5kQ2hpbGQoc2NyaXB0KTs="
+        ></script>
+      </head>
       {/* Aplica la clase de Montserrat al body */}
       <body className={montserrat.className}>
         <ServerLayout params={params}>
