c6abb4608cbd17e4cda9b4941020d7b844c48601
landrescreative <landres.creative@gmail.com>
2024-12-15 19:51:27 -0600

    Solving errors in the sticky banner

--- components/StickyBanner.tsx
+++ components/StickyBanner.tsx
@@ -7,24 +7,24 @@ import { useState } from 'react';
 
 export default function StickyBanner() {
   const [isVisible, setIsVisible] = useState(true);
+  const t = useTranslations('banner');
 
+  // Manejar el cierre del banner
   const handleClose = () => {
     setIsVisible(false);
   };
 
+  // Si no está visible, no renderiza nada
   if (!isVisible) return null;
-
-  const t = useTranslations('banner');
 
   return (
     <div
       id="sticky-banner"
       className="fixed top-0 left-0 z-10 flex justify-between w-full p-1 md:p-4 border-b bg-primary border-gray-600"
-      role="alert"
     >
       <div className="flex items-center mx-auto">
         <p className="flex items-center text-sm font-normal text-white">
-          <span className="inline-flex p-1 mr-3 bg-white rounded-full  w-6 h-6 items-center justify-center flex-shrink-0">
+          <span className="inline-flex p-1 mr-3 bg-white rounded-full w-6 h-6 items-center justify-center flex-shrink-0">
             <svg
               className="w-3 h-3 text-gray-500"
               aria-hidden="true"
@@ -34,7 +34,6 @@ export default function StickyBanner() {
             >
               <path d="M15 1.943v12.114a1 1 0 0 1-1.581.814L8 11V5l5.419-3.871A1 1 0 0 1 15 1.943ZM7 4H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2v5a2 2 0 0 0 2 2h1a2 2 0 0 0 2-2V4ZM4 17v-5h1v5H4ZM16 5.183v5.634a2.984 2.984 0 0 0 0-5.634Z" />
             </svg>
-            <span className="sr-only">Light bulb</span>
           </span>
           <span>{t('span')}</span>
           <Link
